50cf1ed438959785681940cea11693216cb5909e
huotianyu321 <huotianyu321@gmail.com>
2025-05-13 00:00:44 +0800

    temp

---
+++ src/chapter2_light/2_basic_lighting/1_code_box.fs
@@ -0,0 +1,37 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 FragPos; 
+in vec3 Normal;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor; 
+uniform vec3 lightPos; // 光源位置
+uniform vec3 viewPos; // 相机位置
+
+
+void main()
+{
+    // 环境光
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // 漫反射
+    float diffuseStrength = 1.0;
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    // 计算两向量的夹角，代表漫反射强度
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diffuseStrength * diff * lightColor;
+
+    // 镜面反射
+    float specularStrength = 0.5；
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm); 
+    // 镜面反射强度
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    
+    FragColor = vec4((ambient + diffuse + specular) * objectColor, 1.0);
+}
