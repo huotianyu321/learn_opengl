6d5dcd0ea161526fc1c4f6616dec534ccd1563cb
huotianyu321 <huotianyu321@gmail.com>
2025-05-19 09:50:11 +0800

    2-4

---
+++ src/chapter2_light/4_light_texMap/exercise2_reverse_specular.cpp
@@ -0,0 +1,240 @@
+// exercise2
+// 在片段着色器中反转镜面光贴图的颜色值，让木头显示镜面高光而钢制边缘不反光
+
+#include <HEADER/utils.hpp>
+#include <HEADER/shader_class.hpp>
+#include <HEADER/set_uniform.hpp>
+#include <HEADER/process_events.hpp>
+#include <HEADER/camera_class.hpp>
+
+#include <iostream>
+
+typedef unsigned int uint;
+
+const int WIDTH = 1800;
+const int HEIGHT = 1200;
+const char* boxVertexCodePath = "./src/chapter2_light/4_light_texMap/2_box_vs.txt";
+const char* boxFragmentCodePath = "./src/chapter2_light/4_light_texMap/exercise2_box_fs.txt";
+const char* lightFragmentCodePath = "./src/chapter2_light/2_basic_lighting/1_light_fs.txt";
+const char* lightVertexCodePath = "./src/chapter2_light/2_basic_lighting/1_light_vs.txt";
+const char* diffuseTexMapPath = "./resources/container2.png";
+const char* specularTexMapPath = "./resources/container2_specular.png";
+
+Camera camera = Camera(
+	glm::vec3(0.0f, 0.0f, 3.0f), // 初始位置
+	glm::vec3(0.0f, 1.0f, 0.0f), // worldUp
+	-90.0f, // 初始Yaw 偏航角
+	0.0f // 初始Pitch 俯仰角
+);
+
+float lastX = 0;
+float lastY = 0;
+float deltaTime = 0.0f;
+float lastFrameTime = 0.0f;
+bool mouseControlActive = false; // 当鼠标移到窗口中心再激活
+
+void windowSizeChangeCallback(GLFWwindow* window, int newWidth, int newHeight);
+void mouseMoveCallBack(GLFWwindow* window, double xposIn, double yposIn);
+void mouseScrollCallBack(GLFWwindow* window, double xoffset, double yoffset);
+
+int main() {
+
+	GLFWwindow* window = initAndCreateWindow(WIDTH, HEIGHT, "opengl");
+	if (window == nullptr) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, windowSizeChangeCallback);
+	glfwSetScrollCallback(window, mouseScrollCallBack);
+	glfwSetCursorPosCallback(window, mouseMoveCallBack);
+
+	// 前3个是顶点位置，中间三个是法向量， 后两个是纹理坐标
+	float vertices[] = {
+		// positions          // normals           // texture coords
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+	};
+
+	unsigned int locations[] = { 0, 1, 2 };
+	unsigned int dimensions[] = { 3, 3, 2 };
+	unsigned int strides[] = { 8, 8, 8 };
+	unsigned int offsets[] = { 0, 3, 6 };
+
+	VAOData boxVaoData = set_VAO_VBO_EBO_mutiple(
+		vertices, sizeof(vertices), nullptr, 0,
+		3, locations, dimensions, strides, offsets
+	);
+	VAOData lightVaoData = set_VAO_VBO_EBO(
+		vertices, sizeof(vertices), nullptr, 0,
+		0, 3, 8, 0
+	);
+
+	// 光源位置
+	glm::vec3 lightPos(0.0f, 0.2f, 2.0f);
+
+	uint diffuseTexMap = createTexture(diffuseTexMapPath, GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR, true, false);
+	uint specularTexMap = createTexture(specularTexMapPath, GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR, true, false);
+
+	// 光照着色器(应用在立方体上）
+	Shader boxShader(boxVertexCodePath, boxFragmentCodePath);
+	boxShader.use();
+	// 这些没有改变过，就放在loop外边了
+	boxShader.set3Float("light.position", lightPos.x, lightPos.y, lightPos.z);
+	boxShader.set3Float("light.ambient", 0.2f, 0.2f, 0.2f);
+	boxShader.set3Float("light.diffuse", 0.5f, 0.5f, 0.5f);
+	boxShader.set3Float("light.specular", 1.0f, 1.0f, 1.0f);
+	boxShader.setFloat("material.shininess", 32.0f);
+	boxShader.setInt("matierial.diffuse", 0); // 将漫反射贴图纹理采样器绑定到纹理单元0
+	boxShader.setInt("material.specular", 1); // 将镜面反射贴图纹理采样器绑定到纹理单元1
+
+	// 光源着色器
+	Shader lightShader(lightVertexCodePath, lightFragmentCodePath);
+	lightShader.use();
+	lightShader.set3Float("lightColor", 1.0f, 1.0f, 1.0f);
+
+	jobBeforeEnterRenderLoop(window);
+	// 渲染循环
+	while (!glfwWindowShouldClose(window)) {
+		float t = glfwGetTime();
+		float currentFrameTime = t;
+		deltaTime = currentFrameTime - lastFrameTime;
+		lastFrameTime = currentFrameTime;
+
+		jobAtRenderLoopStart(window);
+		processWASD(window, camera, deltaTime);
+
+		// 透视投影矩阵
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f); // 透视投影
+
+		// 观察矩阵
+		glm::mat4 view = camera.GetViewMatrix();
+
+		// box 模型矩阵
+		glm::mat4 boxModel;
+
+		// 绘制box
+		boxShader.use();
+		boxShader.setMat4("projection", projection);
+		boxShader.setMat4("view", view);
+		boxShader.setMat4("model", boxModel);
+		boxShader.set1Vec3("viewPos", camera.Position);
+		boxShader.setInt("material.diffuse", 0);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseTexMap);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, specularTexMap);
+		glBindVertexArray(boxVaoData.VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36); // 绘制立方体
+
+		// 光源 模型矩阵
+		glm::mat4 lightModel;
+		lightModel = glm::translate(lightModel, lightPos);
+		lightModel = glm::scale(lightModel, glm::vec3(0.2f));
+
+		// 绘制光源立方体
+		lightShader.use();
+		lightShader.setMat4("projection", projection);
+		lightShader.setMat4("view", view);
+		lightShader.setMat4("model", lightModel);
+		glBindVertexArray(lightVaoData.VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36); // 绘制光源
+
+		jobBeforRenderLoopEnd(window);
+	}
+
+	glDisable(GL_DEPTH_TEST);
+	glDeleteBuffers(1, &boxVaoData.VBO);
+	glDeleteBuffers(1, &boxVaoData.EBO);
+	glDeleteVertexArrays(1, &boxVaoData.VAO);
+	glDeleteBuffers(1, &lightVaoData.VBO);
+	glDeleteBuffers(1, &lightVaoData.EBO);
+	glDeleteBuffers(1, &boxVaoData.VBO);
+	glfwTerminate();
+	return 0;
+}
+
+
+void mouseMoveCallBack(GLFWwindow* window, double xPosIn, double yPosIn) {
+	// 当前帧的鼠标位置
+	float xPos = static_cast<float>(xPosIn);
+	float yPos = static_cast<float>(yPosIn);
+	std::cout << "X: " << xPos << " Y: " << yPos << std::endl;
+
+	// 让鼠标从外边移动到中心再激活控制
+	if (
+		!mouseControlActive
+		&& xPos >= WIDTH / 2 - 10
+		&& xPos <= WIDTH / 2 + 10
+		&& yPos >= HEIGHT / 2 - 10
+		&& yPos <= HEIGHT / 2 + 10
+		) {
+		mouseControlActive = true; // 鼠标移到窗口中心时激活
+		lastX = xPos;
+		lastY = yPos;
+	}
+
+	if (!mouseControlActive) {
+		return;
+	}
+
+	// 计算鼠标移动的距离
+	float xOffset = xPos - lastX;
+	// 反向y轴，屏幕坐标系是以左上角为原点的. 
+	// 当鼠标向下移动时，y坐标增大，但此时俯仰角应该减小
+	float yOffset = lastY - yPos;
+	// 更新上一帧鼠标位置
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.ProcessMouseMovement(xOffset, yOffset); // 更新相机的朝向
+}
+
+
+void mouseScrollCallBack(GLFWwindow* window, double xoffset, double yoffset) {
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void windowSizeChangeCallback(GLFWwindow* window, int newWidth, int newHeight) {
+	glViewport(0, 0, newWidth, newHeight);
+}
