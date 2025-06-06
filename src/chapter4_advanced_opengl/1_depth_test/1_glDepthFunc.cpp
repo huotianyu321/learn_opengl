edae95c9ece927bcf1c211ec67a7e92b1eb8778d
huotianyu321 <huotianyu321@gmail.com>
2025-06-06 09:31:05 +0800

    chapter4_1_1

---
+++ src/chapter4_advanced_opengl/1_depth_test/1_glDepthFunc.cpp
@@ -0,0 +1,224 @@
+#include <HEADER/utils.hpp>
+#include <HEADER/shader_class.hpp>
+#include <HEADER/set_uniform.hpp>
+#include <HEADER/process_events.hpp>
+#include <HEADER/camera_class.hpp>
+
+#include <iostream>
+
+typedef unsigned int uint;
+
+const int WIDTH = 1800;
+const int HEIGHT = 1200;
+const char* vertexCodePath = "./src/chapter4_advanced_opengl/1_depth_test/1_vs.txt";
+const char* fragmentCodePath = "./src/chapter4_advanced_opengl/1_depth_test/1_fs.txt";
+const char* cudeTexturePath = "./resources/marble.jpg";
+const char* planeTexturePath = "./resources/metal.png";
+
+Camera camera = Camera(
+	glm::vec3(0.0f, 0.0f, 3.0f), // 初始位置
+	glm::vec3(0.0f, 1.0f, 0.0f), // worldUp
+	-90.0f, // 初始Yaw 偏航角
+	0.0f // 初始Pitch 俯仰角
+);
+
+float lastX = WIDTH / 2;
+float lastY = HEIGHT / 2;
+float deltaTime = 0.0f;
+float lastFrameTime = 0.0f;
+float isFirstTimeMouseMove = true;
+
+void windowSizeChangeCallback(GLFWwindow* window, int newWidth, int newHeight);
+void mouseMoveCallBack(GLFWwindow* window, double xposIn, double yposIn);
+void mouseScrollCallBack(GLFWwindow* window, double xoffset, double yoffset);
+
+int main() {
+
+	GLFWwindow* window = initAndCreateWindow(WIDTH, HEIGHT, "opengl");
+	if (window == nullptr) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, windowSizeChangeCallback);
+	glfwSetScrollCallback(window, mouseScrollCallBack);
+	glfwSetCursorPosCallback(window, mouseMoveCallBack);
+
+	// 箱子顶点数据
+	float cubeVertices[] = {
+		// positions          // texture Coords
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	// 平面顶点数据
+	float planeVertices[] = {
+		// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+		 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+		 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+		 5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+	};
+
+	// VAO VBO EBO
+	unsigned int locations[] = { 0, 1 };
+	unsigned int dimensions[] = { 3, 2 };
+	unsigned int strides[] = { 5, 5 };
+	unsigned int offsets[] = { 0, 3 };
+
+	VAOData cubeVaoData = set_VAO_VBO_EBO_mutiple(
+		cubeVertices, sizeof(cubeVertices), nullptr, 0,
+		2, locations, dimensions, strides, offsets
+	);
+	VAOData planeVaoData = set_VAO_VBO_EBO_mutiple(
+		planeVertices, sizeof(planeVertices), nullptr, 0,
+		2, locations, dimensions, strides, offsets
+	);
+
+	// 纹理
+	uint cubeTexture = createTexture(cudeTexturePath, GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR_MIPMAP_LINEAR, false, false);
+	uint planeTexture = createTexture(planeTexturePath, GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR_MIPMAP_LINEAR, true, false);
+
+	// 着色器
+	Shader myShader(vertexCodePath, fragmentCodePath);
+	myShader.use();
+	myShader.setInt("texture1", 0);
+
+
+	// 渲染循环
+	glEnable(GL_DEPTH_TEST);
+	while (!glfwWindowShouldClose(window)) {
+		float t = glfwGetTime();
+		float currentFrameTime = t;
+		deltaTime = currentFrameTime - lastFrameTime;
+		lastFrameTime = currentFrameTime;
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glfwPollEvents();
+		processEscapeKey(window);
+		processWASD(window, camera, deltaTime);
+
+		
+		// 透视投影矩阵
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f); // 透视投影
+
+		// 观察矩阵
+		glm::mat4 view = camera.GetViewMatrix();
+
+
+		// 绘制box
+		myShader.use();
+		myShader.setMat4("projection", projection);
+		myShader.setMat4("view", view);
+
+		// 绘制箱子
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		myShader.setMat4("model", model);
+		glBindVertexArray(cubeVaoData.VAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		// 绘制平面
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		myShader.setMat4("model", model);
+		glBindVertexArray(planeVaoData.VAO);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, planeTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+		glfwSwapBuffers(window);
+	}
+
+	glDisable(GL_DEPTH_TEST);
+	glDeleteBuffers(1, &cubeVaoData.VBO);
+	glDeleteBuffers(1, &cubeVaoData.EBO);
+	glDeleteVertexArrays(1, &cubeVaoData.VAO);
+	glDeleteBuffers(1, &planeVaoData.VBO);
+	glDeleteBuffers(1, &planeVaoData.EBO);
+	glDeleteVertexArrays(1, &planeVaoData.VAO);
+	glfwTerminate();
+	return 0;
+}
+
+
+void mouseMoveCallBack(GLFWwindow* window, double xPosIn, double yPosIn) {
+	// 当前帧的鼠标位置
+	float xPos = static_cast<float>(xPosIn);
+	float yPos = static_cast<float>(yPosIn);
+	// std::cout << "X: " << xPos << " Y: " << yPos << std::endl;
+
+	if (isFirstTimeMouseMove) {
+		lastX = xPos;
+		lastY = yPos;
+		isFirstTimeMouseMove = false;
+	}
+
+	// 计算鼠标移动的距离
+	float xOffset = xPos - lastX;
+	// 反向y轴. 屏幕坐标系是以左上角为原点的, 当鼠标向下移动时，y坐标增大，但此时俯仰角应该减小
+	float yOffset = lastY - yPos;
+
+	// 更新上一帧鼠标位置
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.ProcessMouseMovement(xOffset, yOffset); // 更新相机的朝向
+}
+
+
+void mouseScrollCallBack(GLFWwindow* window, double xoffset, double yoffset) {
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void windowSizeChangeCallback(GLFWwindow* window, int newWidth, int newHeight) {
+	glViewport(0, 0, newWidth, newHeight);
+}
